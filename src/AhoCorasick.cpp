cc9651e466d65f77a6a1b516427e1dedae88c787
Júlia Feitosa <julia.cfeitosa@hotmail.com>
2018-10-17 19:37:35 -0300

    Working, case insensitive Aho Corasick

--- src/AhoCorasick.cpp
+++ src/AhoCorasick.cpp
@@ -4,119 +4,150 @@
 using namespace std;
 #define MAXCHAR 256
 
-void Enter(const std::string &pat, std::vector<vector<int>> &go2, std::vector<int> &occ, int &newstate){
+int GetMaxLength(std::vector<string> patterns){
 
-
-  int state = 0, i = 1, idx = pat[i] - 'a';
-
-  while(go2[state][idx] != -1){
-    idx = pat[i] - 'a';
-    state = go2[state][idx];
-    i++;
+  int res = 0;
+  for(auto p : patterns){
+    res +=p.length();
   }
-
-  for(int j=i; j<pat.length(); j++){
-
-    newstate++;
-    idx = pat[j] - 'a';
-    go2[state][idx] = newstate;
-    state = newstate;
-  }
-  go2.push_back(vector<int>(MAXCHAR, -1));
-
-
+  return res;
 }
 
-void BuildFSM(const std::vector<string> &patterns, std::vector<vector<int>> go2, std::vector<int> &occ){
+void BuildFail(std::vector<vector<int>> &go2, std::vector<int> &occ, std::vector<int> &fail){
 
-cout << "in BuildFSM\n";
-int newstate = 0, k = patterns.size();
-
-for(int i=0; i<k; i++){
-  Enter(patterns[i], go2, occ, newstate);
-  occ[newstate] |= (1 << i);
-}
-for(int i=0; i<MAXCHAR; i++){
-  if(go2[0][i] == -1){
-    go2[0][i] = 0;
-  }
-}
-
-
-}
-
-void BuildFail(std::vector<vector<int>> go2, std::vector<int> &occ, std::vector<int> &fail){
-
-cout << "in Fail\n";
 std::queue<int> q;
-int r, state;
+int state;
 
 for(int i=0; i<MAXCHAR; i++){
   if(go2[0][i] != 0){
-    q.push(go2[0][i]);
+
     fail[go2[0][i]] = 0;
+    q.push(go2[0][i]);
   }
 }
-
 while(!q.empty()){
-  state = q.front();
-  q.pop();
+      state = q.front();
+      q.pop();
+
+      for(int ch=0; ch<MAXCHAR; ch++){
+
+         if(go2[state][ch] != -1){
+            int f = fail[state];
+
+            while(go2[f][ch] == -1){
+               f = fail[f];
+             }
+
+            f = go2[f][ch];
+            fail[go2[state][ch]] = f;
+
+            occ[go2[state][ch]] |= occ[f];
+            q.push(go2[state][ch]);
+         }
+      }
+}
+
+}
+
+
+void BuildFSM(const std::vector<string> &patterns, std::vector<vector<int>> &go2, std::vector<int> &occ){
+
+int state = 1, max_length = GetMaxLength(patterns);
+
+  for(int i=0; i<patterns.size(); i++){
+    int cur = 0, idx;
+    for(int j=0; j<patterns[i].length(); j++){
+        idx = patterns[i][j] - '0' + 26;
+
+        if(go2[cur][idx] == -1){
+          go2[cur][idx] = state++;
+        }
+        cur = go2[cur][idx];
+    }
+    occ[cur] = occ[cur] |(1 << i);
+
+  }
 
   for(int i=0; i<MAXCHAR; i++){
-    if(go2[state][i] != -1){
-
-      q.push(go2[0][i]);
-      state = fail[state];
-
-      while(go2[state][i] == -1){
-        fail[state] = go2[state][i];
-        occ.push_back(occ[fail[go2[0][i]]]);
-      }
+    if(go2[0][i] == -1){
+      go2[0][i] = 0;
     }
   }
 }
 
+int Go2Next(const std::vector<vector<int>> &go2, const std::vector<int> &fail, const char &ch, int state){
+
+int idx = ch - '0' + 26, st = state;
+  while(go2[st][idx] == -1){
+    st = fail[st];
+  }
+  return go2[st][idx];
+
 }
+
+void PrintGo2(vector<vector<int>> &go2){
+  for(int i=0; i<MAXCHAR; i++){
+    for(int j=0; j<MAXCHAR; j++){
+      printf("%d", go2[i][j]);
+  }
+  printf("\n" );
+}
+}
+
+void PrintFail(vector<int> &fail){
+  for(int i=0; i<fail.size(); i++){
+
+      printf("%d\n", fail[i]);}
+}
+
 
 std::vector<int> AhoCorasick(const std::string &text, const std::vector<string> &patterns){
 
 std::vector<vector<int>> go2;
 std::vector<int> occ;
-int state = 0, cur = 0, qtd;
+std::vector<int> fail;
+std::vector<int> res(patterns.size(), 0);
 
-go2.push_back(vector<int>(MAXCHAR, -1));
-occ.push_back(0);
+int state = 0, max_length = GetMaxLength(patterns);
+
+for(int i=0; i<MAXCHAR; i++){
+  go2.push_back(vector<int>(MAXCHAR, -1));
+}
+occ.assign(MAXCHAR, 0);
+fail.assign(MAXCHAR, -1);
 
 BuildFSM(patterns, go2, occ);
-
-std::vector<int> fail(go2.size(), 0);
+//PrintGo2(go2);
 BuildFail(go2, occ, fail);
+//PrintFail(fail);
 
 for(int i=0; i<text.size(); i++){
-  cur = text[i] - 'a';
-  state = go2[state][cur];
-  qtd += occ[state];
 
-  if(occ[state] != 0){
-    for (int j = 0; j < patterns.size(); ++j)
-        {
-            if (occ[state] & (1 << j))
-            {
-                cout << "Word " << patterns[j] << " appears from "
-                     << i - patterns[j].size() + 1 << " to " << i << endl;
-            }
-        }
+  state = Go2Next(go2, fail, text[i], state);
+  if(occ[state] == 0)
+    continue;
+
+  for (int j = 0; j < patterns.size(); j++){
+    if (occ[state] & (1 << j)){
+      //cout << "found: " << patterns[j] << " at " << i-patterns[j].size()+1 << endl;
+              res[j]++;
+    }
   }
 }
-return occ;
+return res;
 }
 
 
 int main(){
-  std::string text = "she sells sea shells on the sea shore";
+  std::string text = "She sells seashells by the seashore.The shells she sells are surely seashells.So if she sells shells on the seashore,I'm sure she sells seashore shells.";
   std::vector<string> patterns  = {"he", "she", "hers", "his"};
+  std::vector<int> res;
 
-  AhoCorasick(text, patterns);
+  res = AhoCorasick(text, patterns);
+  cout << "case insensitive algorithm" << endl;
+  for(int i=0; i<patterns.size(); i++){
+    cout << res[i] << " occurences of " << patterns[i] << endl;
+  }
 
     return 0;
 }
