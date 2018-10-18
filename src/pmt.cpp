a4dc410d8888fb5fcc71b868dc999aa59bbed7f7
Unknown <junior.lima.ivd.2@gmail.com>
2018-10-18 09:05:12 -0300

    Started parsing program arguments
    MIssing file reader to finish the parsing.

--- src/pmt.cpp
+++ src/pmt.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 
+
 using namespace std;
 
 bool IsInArray(string needle, vector<string> haystack){
@@ -13,11 +14,49 @@ bool IsInArray(string needle, vector<string> haystack){
       return false;
 }
 
+enum Algorithm
+{
+      NONE,
+      BOYER_MOORE,
+      AHO_CORASICK,
+      SELLERS,
+      WU_MAMBER
+};
+
+typedef struct RunInfo {
+      vector<string> patterns;
+      vector<string> textFiles;
+      int distance;
+      bool isExact;
+      bool isCountMode;
+      Algorithm chosenAlgorithm;
+
+} RunInfo;
+
+vector<string> parserPatternFile(string filename){
+
+}
+
+Algorithm chooseAlgorithm(RunInfo info) {
+      return AHO_CORASICK;
+}
+
+void executeAlgorithm(RunInfo info){
+
+}
+
 int main(int argc, char *argv[])
 {
       vector<string> pmt_algorithms = {"boyer-moore", "aho-corasick", "sellers", "wu-manber"};
       int opt, opt_index; /* opt = value returned by the getopt_long function | opt_index = index of the chosen option, stored by the getopt_long function */
       string algorithm;
+      RunInfo info;
+      info.chosenAlgorithm = NONE;
+      info.distance = -1;
+      info.isCountMode = false;
+      info.isExact = true;
+      info.patterns.clear();
+      info.textFiles.clear();
 
       /* {long option name, argument, flag value, short option name} */
       static struct option long_options[]{
@@ -34,10 +73,18 @@ int main(int argc, char *argv[])
 
                   case 'a':
                   {
-                        printf("option -a with arg %s\n", optarg);
+                        //printf("option -a with arg %s\n", optarg);
                         algorithm = optarg;
-                        if(!IsInArray(algorithm, pmt_algorithms)){
-                             printf("Invalid algorithm name.\n");
+                        
+                        for(int i=0; i < pmt_algorithms.size(); i++){
+                              if (pmt_algorithms[i] == algorithm) {
+                                    info.chosenAlgorithm = Algorithm(i+1);
+                              }      
+                        }
+
+                        if (info.chosenAlgorithm == NONE) {
+                              printf("Invalid algorithm name.\n");
+                              exit(0);
                         }
                   
                   break;
@@ -45,11 +92,16 @@ int main(int argc, char *argv[])
 
                   case 'c':                  
                         printf("option -c\n");
+                        info.isCountMode = true;
                   break;
 
                   
-                  case 'e':                  
-                        printf("option -e with arg %s\n", optarg);                  
+                  case 'e':
+                        int distance;                  
+                        printf("option -e with arg %s\n", optarg);
+                        sscanf(optarg, "%d", &distance);
+                        info.isExact = false;
+                        info.distance = distance;                  
                   break;
 
                   case 'h':
@@ -60,18 +112,39 @@ int main(int argc, char *argv[])
                               "-e, --edit [e_max]\n"
                               "-h, --help\n"
                               "-p, --pattern [pattern_file]\n");
+                        exit(0);
+                        break;
                   break;
 
                   case 'p':                  
-                        printf("option -p with arg %s\n", optarg);                  
+                        printf("option -p with arg %s\n", optarg);
+                        info.patterns = parserPatternFile(optarg);                  
                   break;
 
                   case '?':
                         printf("Invalid argument. Please try again.\n");
+                        exit(0);
                   break;
+                  default:
+                        abort();
             }
 
       }
+
+      int neededArgs = (info.patterns.empty()) ? 2 : 1;
+
+      if (argc - optind < neededArgs) {
+            printf("Not enought arguments.\n");
+            exit(0);
+      }
+
+      if(info.patterns.empty()) info.patterns.push_back(argv[optind++]);
+
+      for(; optind < argc; optind++) info.textFiles.push_back(argv[optind]);
+
+      if (info.chosenAlgorithm == NONE) info.chosenAlgorithm = chooseAlgorithm(info);
+
+      executeAlgorithm(info);
 
       return 0;
 }
